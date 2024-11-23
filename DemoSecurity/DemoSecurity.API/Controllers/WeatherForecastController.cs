d8e59a94326d539ddc15c669631be287d8bd4032
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-11-23 12:29:08 +0100

    funziona

--- DemoSecurity/DemoSecurity.API/Controllers/WeatherForecastController.cs
+++ DemoSecurity/DemoSecurity.API/Controllers/WeatherForecastController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoSecurity.API.Controllers
 {
     [ApiController]
+    [Authorize()]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
